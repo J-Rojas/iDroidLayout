542aa37cab758b1e45f7c798f143ac379c5783cb
J-Rojas <jose@sano.co>
2015-03-05 19:50:38 -0800

    Expose inflater through IDLTableViewCell for actionTarget usage.

--- iDroidLayout/View/IDLTableViewCell.h
+++ iDroidLayout/View/IDLTableViewCell.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 #import "IDLLayoutBridge.h"
 
+@class IDLLayoutInflater;
+
 @interface IDLTableViewCell : UITableViewCell
 
 @property (weak, nonatomic, readonly) IDLLayoutBridge *layoutBridge;
